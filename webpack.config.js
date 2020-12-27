5e816e73eeef19ee4c2c9956f7b7d6fb28c88b29
jasmine-jp <61972863+jasmine-jp@users.noreply.github.com>
2020-12-27 20:58:44 +0900

    hoge

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = (env, argv) => {
     },
     devServer: {
       contentBase: 'www',
-      port: 8080
+      port: process.env.PORT || 5000
     },
     resolve: {
       extensions: ['.ts', '.js'],
